fd87bfc7e9675f451766eeb44653316f2f54e288
Gabriel Mota Bromonschenkel Lima <gabriel.mota.b.lima@gmail.com>
2023-10-31 16:33:26 -0300

    :bug: fix server

--- server.py
+++ server.py
@@ -3,12 +3,18 @@
 import gradio as gr # type: ignore
 from interfaces import (
 	text_to_speech  as ts,
-	speech_to_text  as st
+	speech_to_text  as st,
+    spaces
 )
 
 if __name__ == "__main__":
+    spaces_list = ["ydshieh/Kosmos-2"]
+    spaces_interfaces = [spaces.spaces(space) for space in spaces_list]
+
     app = gr.TabbedInterface([ts.text_to_speech(),
-                              st.speech_to_text()],
+                              st.speech_to_text(),
+                             *spaces_interfaces],
                              ["Text-to-Speech",
-                              "Speech-to-Text"])
+                              "Speech-to-Text",
+                              *spaces_list])
     app.queue(max_size=20).launch(server_name="0.0.0.0", server_port=7860)
