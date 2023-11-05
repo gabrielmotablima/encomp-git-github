17e1329d2c84ee0e724e101787246e470d595aa4
Gabriel Mota Bromonschenkel Lima <gabriel.mota.b.lima@gmail.com>
2023-11-04 22:49:05 -0300

    :memo: add docstring and lint fixing

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+"""Module providing the unit test class and methods."""
+
+
 import unittest
 
 import gradio as gr
@@ -9,11 +12,24 @@ from interfaces import (
 )
 
 class TestInterfaces(unittest.TestCase):
+    """ Class intended for unit testing of interfaces
+ 
+        Attibutes
+        ----------
+            text_to_speech
+                text to speech interface
+
+        Methods
+        -------
+            test_is_instance()
+                test if the instances of the interfaces are correct
+    """
     text_to_speech = ts.text_to_speech()
     speech_to_text = st.speech_to_text()
     spaces = spaces.spaces()
 
     def test_is_instance(self):
+        """ Test if the interfaces instances are correct"""
         self.assertIsInstance(self.text_to_speech, gr.interface.Interface)
         self.assertIsInstance(self.speech_to_text, gr.interface.Interface)
         self.assertIsInstance(self.spaces, gr.blocks.Blocks)
